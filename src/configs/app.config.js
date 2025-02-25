78cd2623a0a7a47520583fe01b81076e5f301e41
Ayaweisoft <43746766+promiseayawei@users.noreply.github.com>
2025-02-25 21:21:02 +0000

    reverted server url

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,5 +1,5 @@
 const appConfig = {
-    apiPrefix: "https://i-sabi-server.onrender.com/api",
+    apiPrefix: "https://i-sabi-server-dev.herokuapp.com/api",
     
     //apiPrefix: "http://localhost:8000/api",
     devAppUrl: "http://localhost:5173/",
